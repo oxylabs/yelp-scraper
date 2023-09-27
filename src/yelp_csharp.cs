6d5e3e6167cee71d9dc15d00c6276de2ff153c5b
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-09-27 16:25:26 +0300

    Create yelp_csharp.cs

---
+++ src/yelp_csharp.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "USERNAME";
+            const string Password = "PASSWORD";
+
+            var parameters = new Dictionary&lt;string, string>()
+            {
+		{"source", "universal_ecommerce"},
+		{"url", "https://www.yelp.co.uk/search?find_desc=Burgers&find_loc=Frankfurt,%20Hessen,%20Germany"}
+            };
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
